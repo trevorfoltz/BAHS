d6f5ce110000c5ba59bbd3dda8b9b67df022ca04
Trevor Foltz <trevorfoltz@gmail.com>
2013-10-28 11:37:19 -0700

    Initial Commit

---
+++ BAHS/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BAHS
+//
+//  Created by Trevlord on 10/28/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BAHS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BAHS
+//
+//  Created by Trevlord on 10/28/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
