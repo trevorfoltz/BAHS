6a9127c206a62fa25e3635adca644ee9003ad7ed
Trevor Foltz <trevorfoltz@gmail.com>
2013-10-28 14:19:49 -0700

    First commit?

--- BAHS/AppDelegate.m
+++ BAHS/AppDelegate.m
@@ -12,16 +12,26 @@
 
 @implementation AppDelegate
 
+@synthesize navigationController;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    self.window.backgroundColor = [UIColor blackColor];
+    // Override point for customization after application launch.
+    
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil];
     } else {
         self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil];
     }
-    self.window.rootViewController = self.viewController;
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:self.viewController];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    
+    self.window.rootViewController = self.navigationController;
+//    self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
